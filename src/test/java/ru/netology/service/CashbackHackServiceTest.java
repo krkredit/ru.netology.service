7068988ce0ccbb50839844ba19b204f0948ebdaa
Alexandr Astahov <kr_kredit@mail.ru>
2020-03-04 10:14:34 +0300

    change CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -8,38 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-//    @Test
-//    void chouldReturn100IfAmount900() {
-//        CashbackHackService service = new CashbackHackService();
-//        int amount = 900;
-//
-//        int actual = service.remain(amount);
-//        int expected = 100;
-//
-//        assertEquals(actual, expected);
-//    }
-//
-//    @Test
-//    void chouldReturnZeroIfAmount1000() {
-//        CashbackHackService service = new CashbackHackService();
-//        int amount = 1000;
-//
-//        int actual = service.remain(amount);
-//        int expected = 0;
-//
-//        assertEquals(actual, expected);
-//    }
-//
-//    @Test
-//    void chouldReturn800IfAmount1200() {
-//        CashbackHackService service = new CashbackHackService();
-//        int amount = 1200;
-//
-//        int actual = service.remain(amount);
-//        int expected = 800;
-//
-//        assertEquals(actual, expected);
-//    }
 
     @ParameterizedTest // вместо @Test
     @CsvFileSource(resources = "/CashbackHackServiceTest.csv", numLinesToSkip = 1)
